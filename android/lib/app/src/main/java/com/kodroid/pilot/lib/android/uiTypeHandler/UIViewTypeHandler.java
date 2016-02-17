b7c6e5987b43199b45e24c15fab3393da220565d
Jaco <dori@kodroid.com>
2016-02-17 15:08:59 +0000

    AnimatingDisplayer

--- android/lib/app/src/main/java/com/kodroid/pilot/lib/android/uiTypeHandler/UIViewTypeHandler.java
+++ android/lib/app/src/main/java/com/kodroid/pilot/lib/android/uiTypeHandler/UIViewTypeHandler.java
@@ -1,5 +1,7 @@
 package com.kodroid.pilot.lib.android.uiTypeHandler;
 
+import android.animation.Animator;
+import android.animation.ObjectAnimator;
 import android.content.Context;
 import android.view.View;
 import android.widget.FrameLayout;
@@ -10,7 +12,9 @@ import com.kodroid.pilot.lib.stack.PilotFrame;
 import com.kodroid.pilot.lib.sync.UITypeHandler;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 public class UIViewTypeHandler implements UITypeHandler
 {
@@ -197,6 +201,184 @@ public class UIViewTypeHandler implements UITypeHandler
         {
             //simple as setCurrentView is simple and non-animating (i.e. only ever one view present at a time)
             return mRootViewGroup.getChildAt(0);
+        }
+    }
+
+    /**
+     * Animator that allows Animator defs to be used to declare view entry / exit animations.
+     *
+     * Note that if a View is scheduled to be removed and therefore being animated out by this class its
+     * backing frame will no longer be on the stack. Make sure a view does not attempt to call into its
+     * backing frame without checking this first otherwise button presses on out animations will crash.
+     *
+     * A LayoutTransition object would've been useful here if it did not cancel running animations when
+     * Views are added / removed.
+     *
+     * This class can be improved by reversing the in anim if still running when the view is removed.
+     * Alternatively Views should be visible for at least the min time that equals the in anim IN duration.
+     */
+    public static class AnimatingDisplayer implements UIViewTypeHandler.Displayer
+    {
+        //==================================================================//
+        // Builder
+        //==================================================================//
+
+        /**
+         * Sets up a {@link UIViewTypeHandler.Displayer} with simple fade in/out Animators.
+         *
+         * As mentioned in the class docs your Views should probably be around for at least
+         * <code>duration</code>*2 otherwise quick transitions may cause unsightly animation combinations.
+         *
+         * @param rootViewGroup
+         * @return
+         */
+        public static AnimatingDisplayer buildWithFade(FrameLayout rootViewGroup, int duration)
+        {
+            ObjectAnimator in = new ObjectAnimator();
+            in.setFloatValues(0, 0, 1);
+            //not using startDelay as no fillBefore
+            in.setDuration(duration*2);
+            in.setPropertyName("alpha");
+
+            ObjectAnimator out = new ObjectAnimator();
+            out.setFloatValues(1, 0);
+            out.setDuration(duration);
+            out.setPropertyName("alpha");
+
+            return new AnimatingDisplayer(rootViewGroup, in, out);
+        }
+
+        //==================================================================//
+        // Fields
+        //==================================================================//
+
+        private Set<View> animatingOutViews = new HashSet<>();
+
+        private FrameLayout rootViewGroup;
+        private final Animator animateIn;
+        private final Animator animateOut;
+
+        //==================================================================//
+        // Constructor
+        //==================================================================//
+
+        public AnimatingDisplayer(
+                FrameLayout rootViewGroup,
+                Animator animateIn,
+                Animator animateOut)
+        {
+            this.rootViewGroup = rootViewGroup;
+            this.animateIn = animateIn;
+            this.animateOut = animateOut;
+        }
+
+        //==================================================================//
+        // Super Interface
+        //==================================================================//
+
+        @Override
+        public boolean isViewVisibleForFrame(Class<? extends PilotFrame> frameClass)
+        {
+            if(getCurrentView() == null) return false;
+            final PilotFrameLayout currentView = (PilotFrameLayout) getCurrentView();
+            return BackedByFrameUtils.getPilotFrameClass(currentView.getClass()).equals(frameClass);
+        }
+
+        @Override
+        public void makeVisible(View newView)
+        {
+            setCurrentView(newView);
+        }
+
+        @Override
+        public Context getViewContext()
+        {
+            return rootViewGroup.getContext();
+        }
+
+        @Override
+        public void clearAllUI() {
+            animateOutViews(0);
+        }
+
+        //==================================================================//
+        // Private
+        //==================================================================//
+
+        /**
+         * Subclasses could override this if want to specify animations etc. Need to be careful that
+         * any overrides don`t introduce any race conditions that can arise from quick succession of
+         * stack transitions. I.e. ensure that {@link #getCurrentView()} always does return the latest
+         * view that has been added / set to be displayed.
+         *
+         * @param newView
+         */
+        private void setCurrentView(final View newView)
+        {
+            //animate in
+            rootViewGroup.addView(newView, 0);
+            Animator animInClone = animateIn.clone();
+            animInClone.setTarget(newView);
+            animInClone.start();
+
+            //animate out all others
+            animateOutViews(1);
+        }
+
+        private void animateOutViews(int startingIndex)
+        {
+            //animate all others out
+            for(int i = startingIndex; i < rootViewGroup.getChildCount(); i++)
+            {
+                final View oldView = rootViewGroup.getChildAt(i);
+
+                if(animatingOutViews.contains(oldView))
+                    continue;
+                else
+                    animatingOutViews.add(oldView);
+
+                Animator animOutClone = animateOut.clone();
+                animOutClone.setTarget(oldView);
+                animOutClone.addListener(new Animator.AnimatorListener()
+                {
+                    @Override
+                    public void onAnimationStart(Animator animation){}
+
+                    @Override
+                    public void onAnimationEnd(Animator animation)
+                    {
+                        removeView();
+                    }
+
+                    @Override
+                    public void onAnimationCancel(Animator animation)
+                    {
+                        removeView();
+                    }
+
+                    @Override
+                    public void onAnimationRepeat(Animator animation){}
+
+                    private void removeView()
+                    {
+                        animatingOutViews.remove(oldView);
+                        if (rootViewGroup.indexOfChild(oldView) >= 0)
+                            rootViewGroup.removeView(oldView);
+                    }
+                });
+                animOutClone.start();
+            }
+        }
+
+        /**
+         * If overriding see {@link #setCurrentView(View)}
+         *
+         * @return
+         */
+        private View getCurrentView()
+        {
+            //we are always adding new views at 0 so we will just look at this
+            return rootViewGroup.getChildAt(0);
         }
     }
 }
