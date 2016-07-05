3aadf247e15c95ef0cb7a67fd2ba4c251c52d9f3
Jaco <dori@kodroid.com>
2016-07-05 19:11:59 +0100

    PilotLifecycleManager interface refactor - can supply args for starting frame

--- android/lib/app/src/main/java/com/kodroid/pilot/lib/android/PilotLifecycleManager.java
+++ android/lib/app/src/main/java/com/kodroid/pilot/lib/android/PilotLifecycleManager.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.kodroid.pilot.lib.stack.Args;
 import com.kodroid.pilot.lib.stack.PilotFrame;
 import com.kodroid.pilot.lib.stack.PilotStack;
 import com.kodroid.pilot.lib.sync.PilotUISyncer;
@@ -16,9 +17,11 @@ import com.kodroid.pilot.lib.sync.PilotUISyncer;
  */
 public class PilotLifecycleManager
 {
-    private final Class<? extends PilotFrame> launchFrameClass;
     private PilotStack pilotStack;
     private PilotUISyncer pilotUISyncer;
+    private final Class<? extends PilotFrame> launchFrameClass;
+    private final Args launchFrameArgs;
+    private final PilotStack.StackEmptyListener stackEmptyListener;
 
     //==================================================================//
     // Constructor
@@ -26,12 +29,23 @@ public class PilotLifecycleManager
 
     /**
      * @param pilotStack the PilotStack instance to be managed by this class.
-     * @param launchFrameClass The launch frame for the handled PilotStack. Will only be created on first creation of the stack.
+     * @param pilotUISyncer will be added to the backing PilotStack and removed in onDestory() (and reference nulled). This will be updated with the current stack state inside this method.
+     * @param launchFrameClass The launch frame for the handled PilotStack. Will only be created on first creation of the stack. Will be ignored if a stack exists in the savedInstanceState
+     * @param launchFrameArgs Args to be passed to the launch frame. Can be null.
+     * @param stackEmptyListener to be notified when the stack becomes empty. Integrators will typically want to exit the current Activity at this point.
      */
-    public PilotLifecycleManager(PilotStack pilotStack, Class<? extends PilotFrame> launchFrameClass)
+    public PilotLifecycleManager(
+            PilotStack pilotStack,
+            PilotUISyncer pilotUISyncer,
+            Class<? extends PilotFrame> launchFrameClass,
+            Args launchFrameArgs,
+            PilotStack.StackEmptyListener stackEmptyListener)
     {
         this.pilotStack = pilotStack;
+        this.pilotUISyncer = pilotUISyncer;
         this.launchFrameClass = launchFrameClass;
+        this.launchFrameArgs = launchFrameArgs;
+        this.stackEmptyListener = stackEmptyListener;
     }
 
     //==================================================================//
@@ -45,17 +59,12 @@ public class PilotLifecycleManager
      * Pilot will not have had a chance to be recreated and therefore will be duplicated.
      *
      * @param savedInstanceState forward the activity's save state bundle here for auto pilot stack state restoration on process death (only)
-     * @param pilotUISyncer will be added to the backing PilotStack and removed in onDestory() (and reference nulled). This will be updated with the current stack state inside this method.
-     * @param stackEmptyListener to be notified when the stack becomes empty. Integrators will typically want to exit the current Activity at this point.
+
      */
-    public void onCreateDelegate(
-            Bundle savedInstanceState,
-            PilotUISyncer pilotUISyncer,
-            PilotStack.StackEmptyListener stackEmptyListener)
+    public void onCreateDelegate(Bundle savedInstanceState)
     {
-        this.pilotUISyncer = pilotUISyncer;
         if(pilotStack.isEmpty())
-            initializePilotStack(savedInstanceState, launchFrameClass);
+            initializePilotStack(savedInstanceState, launchFrameClass, launchFrameArgs);
         else if(!pilotStack.doesContainVisibleFrame())
             throw new IllegalStateException("Trying to initiate UI with a stack that contains no visible frames!");
 
@@ -129,8 +138,9 @@ public class PilotLifecycleManager
      *
      * @param savedInstanceState
      * @param launchFrameClass
+     * @param launchFrameArgs can be null
      */
-    private void initializePilotStack(Bundle savedInstanceState, final Class<? extends PilotFrame> launchFrameClass)
+    private void initializePilotStack(Bundle savedInstanceState, final Class<? extends PilotFrame> launchFrameClass, Args launchFrameArgs)
     {
         if(!pilotStack.isEmpty())
             throw new IllegalStateException("PilotStack already exists!");
@@ -149,7 +159,7 @@ public class PilotLifecycleManager
             //set launch frame
             try
             {
-                pilotStack.pushFrame(launchFrameClass);
+                pilotStack.pushFrame(launchFrameClass, launchFrameArgs);
             }
             catch (Exception e)
             {
